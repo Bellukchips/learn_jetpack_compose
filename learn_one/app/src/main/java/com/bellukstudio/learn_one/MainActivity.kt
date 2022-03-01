01c9ff3f823d4c37c1d74669cfb75fcbb98aac0e
bellukchips <bellukchips@gmail.com>
2022-03-01 20:21:26 +0700

    try modifier padding

--- learn_one/app/src/main/java/com/bellukstudio/learn_one/MainActivity.kt
+++ learn_one/app/src/main/java/com/bellukstudio/learn_one/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bellukstudio.learn_one.ui.theme.Learn_oneTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,11 +28,11 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun App(){
-    Surface(){
+    Surface(color = MaterialTheme.colors.primary){
         Greeting(name = "Belluk")
     }
 }
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
